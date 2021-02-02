bd4c809d3a3b648cbca7a88f4906a2e58aab40ed
rdmaranville <rdmaranville@alaska.edu>
2021-02-01 17:43:12 -0900

    Help, I need somebody, HELP

--- source/TDDKata.cpp
+++ source/TDDKata.cpp
@@ -3,9 +3,37 @@
 int TDDKata(std::string str)
 {
 	std::string delimiter1 = ",", delimiter2 = "\n";
+	std::string temp = "";
+	int num[3], arrpos = 0, total = 1;
 	if (str == "")
 	{
 		return 0;
 	}
-	return 1;
+	
+	for(int loopt = 0; loopt < str.length(); loopt++)
+	{
+		if (str[loopt] == delimiter1[0] || (str[loopt] == delimiter2[0] && str[loopt+1] == delimiter2[1]))
+		{
+			num[arrpos] = std::stoi(temp);
+			temp = "";
+			arrpos++;
+			if (str[loopt + 1] == delimiter2[1])
+			{
+				loopt++;
+			}
+		}
+		else
+		{
+			temp += str[loopt];
+		}
+	}
+	arrpos = 0;
+
+	while (num[arrpos] != NULL)
+	{
+		total += num[arrpos];
+		arrpos++;
+	}
+
+	return total;
 }
