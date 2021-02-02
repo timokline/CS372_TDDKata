434cd0e1b0d6e3c9430853624e07473a4cb53c02
Andrew <arkozak@alaska.edu>
2021-02-01 22:38:41 -0900

    ADDED COMMENTS and am done

--- source/TDDKata.cpp
+++ source/TDDKata.cpp
@@ -15,42 +15,42 @@ int TDDKata(std::string str)
 	//Finding delimiters
 	for (int i = 0; i < str.length(); i++)
 	{
-		if (isdigit(str[i]))
+		if (isdigit(str[i])) //Stoping search for delimiters if a digit is found
 		{
 			break;
 		}
-		if (str[0] == '/' && str[1] == '/')
+		if (str[0] == '/' && str[1] == '/') // Searching for "//" to signify new delimiter
 		{
 			if (str[2] == '[') //Multi-Character
 			{
-				for (int j = i + 3; j < str.length(); j++)
+				for (int j = i + 3; j < str.length(); j++)//Searching for the end of the delimiter
 				{
-					if (str[j] == ']')
+					if (str[j] == ']') //Detects end of delimiter
 					{
-						delim.push_back(temp);
-						start += temp.length() + 4;
+						delim.push_back(temp); //Adds delimiter to the vector of the delimiters
+						start += temp.length() + 4; //Moves start
 						i += temp.length() + 3;
 						temp = "";
 						j = str.length();
 					}
 					else
-						temp.push_back(str[j]);
+						temp.push_back(str[j]); //Adds characters to the delimiter
 				}
 			}
 			else //Single-Character
 			{
-				temp.push_back(str[2]);
+				temp.push_back(str[2]); //Adding the delimiter to the string
 				delim.push_back(temp);
-				start += 3;
+				start += 3; //Pushing the start value back to account for single character delimiters without brakets
 				temp = "";
-				break;
+				break; //Exiting the for loop cause no brackets only means 1 delimiter
 			}
 			
 		}
 
 	}
 	
-	//Extracting numbers with delimeters
+	//Extracting numbers with delimiters
 	for (int i = start; i < str.length(); i++)
 	{
 		if (isdigit(str[i])) //If the character is a digit
@@ -61,21 +61,19 @@ int TDDKata(std::string str)
 
 		for (auto& n : delim) //Checking for delimeters
 		{
-			if (str[i] == n[0])
+			if (str[i] == n[0]) //Comparing the start of the delimiter
 			{
-				for (int j = 0; j < n.length(); j++)
+				for (int j = 0; j < n.length(); j++) //Cycling through the delimiter and comparing to the string
 				{
-					if (n[j] != str[i + j])
-					{
+					if (n[j] != str[i + j]) //If a part of the delimiter does not match
 						total++;
-					}
 				}
-				if (total == 0)
+				if (total == 0) //If the delimiter matches the string
 				{
 					num.push_back(std::stoi(temp));
 					temp = "";
 				}
-				else
+				else //Reseting total 
 					total = 0;
 			}
 
