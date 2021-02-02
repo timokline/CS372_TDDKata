8e22a05d93714b68a416fe286f9ba8404c59f663
Andrew <arkozak@alaska.edu>
2021-02-01 18:46:49 -0900

    Attempting different approach #3

--- source/TDDKata.cpp
+++ source/TDDKata.cpp
@@ -5,7 +5,9 @@ int TDDKata(std::string str)
 	int count = 0;
 	std::string delimiter1 = ",", delimiter2 = "\n";
 	std::string temp = "";
-	int num[3], arrpos = 0, total = 1;
+	std::vector<int> num;
+	int arrpos = 0, total = 1;
+	
 
 	if (str == "") //An empty string returns zero
 	{
@@ -24,6 +26,7 @@ int TDDKata(std::string str)
 		return std::stoi(str);
 	}
 
+	/*
 	for(int i = 0; i < str.length(); i++)
 	{
 		if (str[i] == delimiter1[0] || str[i] == delimiter2[0])
@@ -41,6 +44,7 @@ int TDDKata(std::string str)
 			temp += str[i];
 		}
 	}
+	num[arrpos] = std::stoi(temp);
 	arrpos = 0;
 
 	while (num[arrpos] != NULL)
@@ -48,8 +52,25 @@ int TDDKata(std::string str)
 		total += num[arrpos];
 		arrpos++;
 	}
+	*/
 	
-	
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (isdigit(str[i]))
+		{
+			temp += str[i];
+		}
+		else if (str[i] == delimiter1[0])
+		{
+			num.push_back(std::stoi(temp));
+			temp = "";
+			arrpos++;
+		}
+	}
+	num.push_back(std::stoi(temp));
+	arrpos = 0;
+
+	total = num.size();
 
 	return total;
 }
--- source/TDDKata.h
+++ source/TDDKata.h
@@ -1,3 +1,4 @@
 #include <string>
+#include <vector>
 
 int TDDKata(std::string str);
